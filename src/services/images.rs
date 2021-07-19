cf21aa7815469d0a1fde178b67e18e1c68e8b4d7
Nerixyz <nero.9@hotmail.de>
2021-07-19 15:47:34 +0200

    fix: only check extensions

--- src/services/images.rs
+++ src/services/images.rs
@@ -1,9 +1,12 @@
+use std::{ffi::OsStr, path::Path};
 use url::Url;
 
 pub fn check_image_url(url: &str) -> anyhow::Result<()> {
     let url = Url::parse(url)?;
-    match url.domain() {
-        Some("i.imgur.com") | Some("i.nuuls.com") => Ok(()),
-        _ => Err(anyhow::Error::msg("Domain not whitelisted")),
+    let path = Path::new(url.path());
+    match path.extension().and_then(|s| s.to_str()) {
+        Some("png") | Some("jpg") | Some("jpeg") | Some("webp") | Some("avif") | Some("jxl")
+        | Some("bmp") | Some("svg") => Ok(()),
+        _ => Err(anyhow::Error::msg("Extension not found or whitelisted")),
     }
 }
